4e57abdf25805a226cc386ba1e61ee7acbf1da54
Kevin Wellenzohn <wellenzohn@ifi.uzh.ch>
2021-04-17 12:47:31 +0200

    Update scalability experiment

--- benchmarks/scalability2.cpp
+++ benchmarks/scalability2.cpp
@@ -4,7 +4,7 @@
 int main_(int argc, char** argv) {
   using Exp = benchmark::ScalabilityExperiment2;
 
-  if (argc < 2) {
+  if (argc < 3) {
     return 1;
   }
 
@@ -12,21 +12,10 @@ int main_(int argc, char** argv) {
   std::sscanf(argv[1], "%zu", &dataset_size);
 
   size_t page_size = 16384;
-  std::string filename = "/local/scratch/wellenzohn/datasets/dataset.16384.partition";
+  std::string filename{argv[2]};
   std::vector<Exp::Dataset> datasets = {
     { filename, page_size,   dataset_size },
   };
-
-  /* std::vector<Exp::Dataset> datasets = { */
-  /*   { filename, page_size,   1'000'000'000 }, */
-  /*   /1* { filename, page_size,  10'000'000'000 }, *1/ */
-  /*   /1* { filename, page_size,  50'000'000'000 }, *1/ */
-  /*   /1* { filename, page_size, 100'000'000'000 }, *1/ */
-  /*   /1* { filename, page_size, 150'000'000'000 }, *1/ */
-  /*   /1* { filename, page_size, 200'000'000'000 }, *1/ */
-  /*   /1* { filename, page_size, 250'000'000'000 }, *1/ */
-  /*   /1* { filename, page_size, 300'000'000'000 }, *1/ */
-  /* }; */
 
   Exp bm(datasets);
   bm.Run();
